067c96b9b68fc0bb3ee69355b0af70be1722468a
Chris Soto <chris.soto@ckeefe-10p.fourwindsinteractive.com>
2019-12-07 14:26:52 -0700

    Added day1 step 2

--- day1/index.ts
+++ day1/index.ts
@@ -1,7 +1,15 @@
 import input from './input.json';
 
-const calculateFuleRequired = (mass: number): number => {
-  return Math.floor(mass / 3) - 2;
+// Step 2
+const calculateFuleRequired = (mass: number, totalFuelRequired = 0): number => {
+  const fuelRequired = Math.floor(mass / 3) - 2;
+
+  if (fuelRequired < 0) {
+    return totalFuelRequired;
+  }
+
+  totalFuelRequired += fuelRequired;
+  return calculateFuleRequired(fuelRequired, totalFuelRequired);
 };
 
 (() => {
@@ -11,3 +19,16 @@ const calculateFuleRequired = (mass: number): number => {
 
   console.log('Total Fuel Needed: ', totalFuelNeeded);
 })();
+
+// Step 1
+// const calculateFuleRequired = (mass: number): number => {
+//   return Math.floor(mass / 3) - 2;
+// };
+
+// (() => {
+//   const totalFuelNeeded = input.reduce((prev: number, next: number) => {
+//     return prev + calculateFuleRequired(next);
+//   }, 0);
+
+//   console.log('Total Fuel Needed: ', totalFuelNeeded);
+// })();
