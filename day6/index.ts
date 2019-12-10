b42a3cc65198c2a0cf0a5778bcf43dbd766a6315
Chris Soto <chris.soto@ckeefe-10p.fourwindsinteractive.com>
2019-12-09 20:50:07 -0700

    Day 6, part 2

--- day6/index.ts
+++ day6/index.ts
@@ -1,5 +1,35 @@
 import input from './input.json';
-// const input = ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L'];
+// const input = [
+//   'COM)B',
+//   'B)C',
+//   'C)D',
+//   'D)E',
+//   'E)F',
+//   'B)G',
+//   'G)H',
+//   'D)I',
+//   'E)J',
+//   'J)K',
+//   'K)L',
+//   'K)YOU',
+//   'I)SAN',
+// ];
+
+const SANTA = 'SAN';
+const YOU = 'YOU';
+
+const mapTotalOrbit = (start: string): {} => {
+  let parentOrbits = {};
+  let parentOrbit = orbitKeys[start];
+  let trips = 0;
+
+  while (parentOrbit) {
+    parentOrbits[parentOrbit] = trips++;
+    parentOrbit = orbitKeys[parentOrbit];
+  }
+
+  return parentOrbits;
+};
 
 const orbitKeys = input.reduce((prev, curr: string) => {
   const [orbitObj, obj] = curr.split(')');
@@ -9,14 +39,42 @@ const orbitKeys = input.reduce((prev, curr: string) => {
   return prev;
 }, {});
 
-let totalOrbits = 0;
-for (let key in orbitKeys) {
-  let parentOrbit = orbitKeys[key];
+const santasOrbits = mapTotalOrbit(SANTA);
+const myTotalOrbits = mapTotalOrbit(YOU);
 
-  while (parentOrbit) {
-    totalOrbits++;
-    parentOrbit = orbitKeys[parentOrbit];
+let totalOrbits: number[] = [];
+
+for (let myOrbit in myTotalOrbits) {
+  const matchingSantaOrbit = santasOrbits[myOrbit];
+  if (matchingSantaOrbit) {
+    totalOrbits.push(matchingSantaOrbit + myTotalOrbits[myOrbit]);
   }
 }
 
-console.log('Total Orbits: ', totalOrbits);
+console.log('Lowest Total Orbits: ', Math.min(...totalOrbits));
+
+/**
+ *
+ * Part 1
+ *
+ */
+// const input = ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L'];
+// const orbitKeys = input.reduce((prev, curr: string) => {
+//   const [orbitObj, obj] = curr.split(')');
+
+//   prev[obj] = orbitObj;
+
+//   return prev;
+// }, {});
+
+// let totalOrbits = 0;
+// for (let key in orbitKeys) {
+//   let parentOrbit = orbitKeys[key];
+
+//   while (parentOrbit) {
+//     totalOrbits++;
+//     parentOrbit = orbitKeys[parentOrbit];
+//   }
+// }
+
+// console.log('Total Orbits: ', totalOrbits);
